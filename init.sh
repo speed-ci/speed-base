e419630a5881ee71ca69dba267fc675559485998
Mathieu Filotto <m.filotto@eramet-sln.nc>
2018-02-20 08:35:52 +1100

    fix(env): use git to extract branch name only if project has commits

--- init.sh
+++ init.sh
@@ -67,7 +67,9 @@ init_git_env () {
     PROJECT_NAMESPACE=${PROJECT_NAMESPACE_URL##*/}
     
     if [[ -z $BRANCH_NAME ]]; then
-        BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+        if [[ ! $(git status | grep "Initial commit")  ]]; then
+            BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+        fi
         BRANCH_NAME=${BRANCH_NAME:-"master"}
     fi
 }
